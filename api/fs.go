6a2594503695ffa94cee03a47a7bf40154446cf6
Kujtim <kujtimii.h@gmail.com>
2017-02-12 23:02:28 +0100

    Update paths.

--- api/fs.go
+++ api/fs.go
@@ -11,30 +11,25 @@ type FsAPI struct {
 }
 
 func (f *FsAPI) ReadFile(path string) (string, error) {
-	path = strings.Replace(path,"\\",f.FileSeparator(),-1)
 	d, err := afero.ReadFile(f.fs, path)
 	return string(d), err
 }
 
 func (f *FsAPI) WriteFile(path string, data string) error {
-	path = strings.Replace(path,"\\",f.FileSeparator(),-1)
 	return afero.WriteFile(f.fs, path, []byte(data), os.ModePerm)
 }
 
 func (f *FsAPI) Mkdir(path string) error {
-	path = strings.Replace(path,"\\",f.FileSeparator(),-1)
 	return f.fs.Mkdir(path, os.ModePerm)
 }
 
 func (f *FsAPI) MkdirAll(path string) error {
-	path = strings.Replace(path,"\\",f.FileSeparator(),-1)
 	return f.fs.MkdirAll(path, os.ModePerm)
 }
 func (f *FsAPI) FileSeparator() string {
 	return afero.FilePathSeparator
 }
 func (f *FsAPI) Exists(path string) (bool, error) {
-	path = strings.Replace(path,"\\",f.FileSeparator(),-1)
 	return afero.Exists( f.fs,path)
 }
 func NewFsAPI(fs afero.Fs) *FsAPI {
